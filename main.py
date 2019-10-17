f1311bb21047dd9f9b5f7302c82de92181e86c01
madhursk <madhur.kabra@spit.ac.in>
2019-10-17 11:49:39 +0530

    Phase 1 final

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from flask_sqlalchemy import SQLAlchemy
@@ -11,11 +11,6 @@ import nltk
 import random
 import pandas as pd
 
-bot = ChatBot('Friend') #create the bot
-
-#bot.set_trainer(ListTrainer) # Teacher
-trainer = ListTrainer(bot)
-#bot.train(conv) # teacher train the bot
 
 app = Flask(__name__)
 # app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
@@ -24,45 +19,38 @@ app.debug = True
 db = SQLAlchemy(app)
 
 
-class Domain_Ext(db.Model):
-	id = db.Column(db.Integer, primary_key= True)
-	#uid = db.Column(db.String(100))
-	question = db.Column(db.String(100))
-	response = db.Column(db.String(100))
-	kw = db.Column(db.String(100))
+# class Domain_Ext(db.Model):
+# 	id = db.Column(db.Integer, primary_key= True)
+# 	#uid = db.Column(db.String(100))
+# 	question = db.Column(db.String(100))
+# 	response = db.Column(db.String(100))
+# 	kw = db.Column(db.String(100))
 
-	def __init__(self, question, response, kw):
-		#self.uid = uid
-		self.question = question
-		self.response = response
-		self.kw = kw
-
-
-	def __repr__(self):
-		return f"Domain_Ext('{self.question}', '{self.response}', '{self.kw}')"
+# 	def __init__(self, question, response, kw):
+# 		#self.uid = uid
+# 		self.question = question
+# 		self.response = response
+# 		self.kw = kw
 
 
+# 	def __repr__(self):
+# 		return f"Domain_Ext('{self.question}', '{self.response}', '{self.kw}')"
 
-class domain_selection(db.Model):
-	id = db.Column(db.Integer, primary_key = True)
-	kw = db.Column(db.String(100))
-	subdomain = db.Column(db.String(100))
 
-	def __repr__(self):
-		return f"domain_selection('{self.kw}', '{self.subdomain}')"
+
+# class domain_selection(db.Model):
+# 	id = db.Column(db.Integer, primary_key = True)
+# 	kw = db.Column(db.String(100))
+# 	subdomain = db.Column(db.String(100))
+
+# 	def __repr__(self):
+# 		return f"domain_selection('{self.kw}', '{self.subdomain}')"
 
 
 
 
 
 ask_phrases = ['Tell me something more about ','Can you elaborate about your work in ', 'This seems interesting... can you tell me more about ', 'What did you do in ']
-
-flimit = 4
-
-for knowledeg in os.listdir('base'):
-	BotMemory = open('base/'+ knowledeg, 'r').readlines()
-	trainer.train(BotMemory)
-
 
 
 
@@ -73,26 +61,15 @@ for knowledeg in os.listdir('base'):
 def index():
 	return render_template('index.html')
 
+
+@app.route('/subdomain', methods=["GET", "POST"])
+def subdomain():
+	return render_template('subdomain.html')
+
+
+
+
 @ app.route('/process', methods=['POST'])
-
-
-
-# def nouns_extract(nouns,ans):
-# 	# ask_phrases = ['Tell me something more about ','Can you elaborate about your work in ', 'This seems interesting... can you tell me more about ', 'What did you do in ']
-# 	words = nltk.word_tokenize(ans)
-# 	list_words = nltk.pos_tag(words)
-# 	for i in range(len(list_words)):
-# 		if("NN" in list_words[i][1]):
-# 			if(i+1<len(list_words)):
-# 				if("NN" in list_words[i+1][1]):
-# 					nouns.append(list_words[i][0]+" "+list_words[i+1][0])
-# 					print(list_words[i][0]+" "+list_words[i+1][0])
-# 			else:
-# 				nouns.append(list_words[i][0])
-# 				print(list_words[i][0])
-# 	return nouns
-
-# flimit = 4
 
 # con = sqlite3.connect('chatbot.db')
 # 			cursorObj = con.cursor()
@@ -110,27 +87,11 @@ def process():
 	con = sqlite3.connect('chatbot.db')
 	cursorObj = con.cursor()
 
-	# cursorObj.execute('SELECT * FROM domain_extraction ORDER BY uid DESC LIMIT 1')
-	# rows = cursorObj.fetchall()
-	# con.commit()
-	# cursorObj.close()
-	# print('Printing rows')
-	# print(rows)
 	ans = request.form['user_input']
 
-	# data = []
-	# for i in rows:
-	# 	for j in i:
-	# 		data.append(j)
-	# # data = list(rows)
-	# print('printing data')
-	# print(data)
+	
 	qtns1 = "Tell me about your recent project"
-	# con = sqlite3.connect('chatbot.db')
-	# cursorObj = con.cursor()
-	# cursorObj.execute('DELETE FROM domain_extraction WHERE uid=(SELECT MAX(uid) FROM domain_extraction)')
-	# con.commit()
-	# cursorObj.close()
+	
 
 	ask_phrases = ['Tell me something more about ',
                'Can you elaborate about your work in ', 
@@ -140,8 +101,7 @@ def process():
 
 	count = 0
 	kw = []
-	# return render_template('index.html',user_input="First answer",bot_response=qtns1)	
-	# ans=request.form['user_input']
+
 	nouns = []
 	n_count=0
 	words = nltk.word_tokenize(ans)
@@ -216,12 +176,7 @@ def process():
 
 	return render_template('index.html',user_input=ans,bot_response=qtn)
 
-	# con = sqlite3.connect('chatbot.db')
-	# cursorObj = con.cursor()
-	# cursorObj.execute("INSERT INTO domain_extraction (question, answer, keyword) VALUES (?,?,?)",(qtn,'',''))
-	# con.commit()
-	# cursorObj.close()
-
+	
 
 
 
